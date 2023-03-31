17439ec53513f39e43985e27bd98049b0cb81e5d
Anurag Unnikannan <anuragunnikannan7@gmail.com>
2023-04-01 02:59:00 +0530

    fixed issue of invalid form when passwords are equal

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let btn = document.querySelector("button");
 btn.addEventListener("click", () => {
-    let pswd = document.querySelector("#pswd").value;
-    let confpswd = document.querySelector("#confpswd").value;
-    if(pswd != confpswd) {
-        document.querySelector("#pswd").setCustomValidity("Invalid");
-        document.querySelector("#confpswd").setCustomValidity("Invalid");
+    let pswd = document.querySelector("#pswd");
+    let confpswd = document.querySelector("#confpswd");
+    if(pswd.value != confpswd.value) {
+        pswd.setCustomValidity("Invalid");
+        confpswd.setCustomValidity("Invalid");
         document.querySelector("#errmsg").innerText = "* Passwords do not match";
+    }
+    else {
+        pswd.setCustomValidity("");
+        confpswd.setCustomValidity("");
+        document.querySelector("#errmsg").innerText = "";
     }
 });
